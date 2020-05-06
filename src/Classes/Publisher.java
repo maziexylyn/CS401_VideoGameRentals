ca047c1e09feeeb2d102e2ecce20c78fb39fc31d
Melinda <m.kwok94@yahoo.com>
2020-05-05 19:25:49 -0700

    Inital commit, redid branch

---
+++ src/Classes/Publisher.java
@@ -0,0 +1,37 @@
+package Classes;
+
+public class Publisher {
+    private int id;
+    private String name;
+    private boolean isActive;
+
+    Publisher(int id, String name, boolean isActive){
+        this.id = id;
+        this.name = name;
+        this.isActive = isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
