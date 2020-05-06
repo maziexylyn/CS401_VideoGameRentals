783587138a3612eabfd68bfab7b3e9785080010e
Melinda <m.kwok94@yahoo.com>
2020-05-06 11:36:14 -0700

    Publisher Class/Genre Class

--- src/Classes/Genre.java
+++ src/Classes/Genre.java
@@ -6,12 +6,12 @@ import java.sql.ResultSet;
 import java.sql.Types;
 import java.util.ArrayList;
 
-public class Publisher {
+public class Genre {
     private int id;
     private String name;
     private boolean isActive;
 
-    Publisher(int id, String name, boolean isActive){
+    Genre(int id, String name, boolean isActive){
         this.id = id;
         this.name = name;
         this.isActive = isActive;
@@ -41,18 +41,18 @@ public class Publisher {
         isActive = active;
     }
 
-
-    public static Publisher read(Connection conn, int publisher_id) {
-        Publisher temp = null;
+    public static Genre read(Connection conn, int genre_id)
+    {
+        Genre temp = null;
 
         try{
             if(conn != null){
-                CallableStatement stmt = conn.prepareCall("CALL Publisher_Read(?)");
-                stmt.setInt(1, publisher_id);
+                CallableStatement stmt = conn.prepareCall("CALL Genre_Read(?)");
+                stmt.setInt(1, genre_id);
                 ResultSet rs = stmt.executeQuery();
 
                 while(rs.next()){
-                    temp = new Publisher(
+                    temp = new Genre(
                             rs.getInt("id"),
                             rs.getString("name"),
                             rs.getBoolean("isActive")
@@ -67,19 +67,18 @@ public class Publisher {
         return temp;
     }
 
-
-    public static Publisher[] readList(Connection conn, boolean publisher_isActive)
+    public static Genre[] readList(Connection conn, boolean genre_isActive)
     {
-        ArrayList<Publisher> publishers = new ArrayList<>();
+        ArrayList<Genre> genres = new ArrayList<>();
 
         try{
             if(conn != null){
-                CallableStatement stmt = conn.prepareCall("CALL Publisher_Read_List(?)");
-                stmt.setBoolean(1, publisher_isActive);
+                CallableStatement stmt = conn.prepareCall("CALL Genre_Read_List(?)");
+                stmt.setBoolean(1, genre_isActive);
                 ResultSet rs = stmt.executeQuery();
 
                 while(rs.next()){
-                    publishers.add(new Publisher(
+                    genres.add(new Genre(
                             rs.getInt("id"),
                             rs.getString("name"),
                             rs.getBoolean("isActive")
@@ -91,18 +90,18 @@ public class Publisher {
             error.printStackTrace();
         }
 
-        return publishers.toArray(new Publisher[0]);
+        return genres.toArray(new Genre[0]);
     }
 
 
-    public static boolean create(Connection conn, String publisher_name)
+    public static boolean create(Connection conn, String genre_name)
     {
         boolean isCreated = false;
 
         try{
             if(conn != null){
-                CallableStatement stmt = conn.prepareCall("CALL Publisher_Create(?, ?)");
-                stmt.setString(1, publisher_name);
+                CallableStatement stmt = conn.prepareCall("CALL Genre_Create(?, ?)");
+                stmt.setString(1, genre_name);
                 stmt.registerOutParameter(2, Types.TINYINT);
                 stmt.execute();
                 isCreated = stmt.getBoolean(2);
@@ -114,17 +113,16 @@ public class Publisher {
         return isCreated;
     }
 
-
-    public static boolean update(Connection conn, int publisher_id, String publisher_name, boolean publisher_isActive)
+    public static boolean update(Connection conn, int genre_id, String genre_name, boolean genre_isActive)
     {
         boolean isUpdated = false;
 
         try{
             if(conn != null){
-                CallableStatement stmt = conn.prepareCall("CALL Publisher_Update(?, ?, ?, ?)");
-                stmt.setInt(1, publisher_id);
-                stmt.setString(2, publisher_name);
-                stmt.setBoolean(3, publisher_isActive);
+                CallableStatement stmt = conn.prepareCall("CALL Genre_Update(?, ?, ?, ?)");
+                stmt.setInt(1, genre_id);
+                stmt.setString(2, genre_name);
+                stmt.setBoolean(3, genre_isActive);
                 stmt.registerOutParameter(4, Types.TINYINT);
                 stmt.execute();
                 isUpdated = stmt.getBoolean(4);
@@ -136,3 +134,4 @@ public class Publisher {
         return isUpdated;
     }
 }
+
